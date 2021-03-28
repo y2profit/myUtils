d4c397cd249c6c36745b72f2a0cae1b6ff89b0e6
yyd0731 <yyd0731@gmail.com>
2021-03-28 17:41:22 +0900

    Initial commit

---
+++ src/test/java/com/zerodk31/myUtils/MyUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerodk31.myUtils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
